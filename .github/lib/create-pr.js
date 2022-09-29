9ee68c8aa5b034bc9a767b76a10c8159826698cf
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-09-29 18:24:19 +0200

    Fix create-pr script

--- .github/lib/create-pr.js
+++ .github/lib/create-pr.js
@@ -1,13 +1,13 @@
-module.exports = async ({ github }) => {
+module.exports = async ({ github, runNumber, providerName }) => {
     const { GITHUB_SERVER_URL, GITHUB_REPOSITORY, GITHUB_RUN_ID } = process.env
     const url = `${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}`
-    const repo = "cdktf-provider-${{ matrix.provider }}"
+    const repo = `cdktf-provider-${providerName}`
     const owner = 'cdktf'
 
     const { data } = await github.pulls.create({
         owner,
         repo,
-        head: "upgrade-provider-project-${{ github.run_number }}",
+        head: `upgrade-provider-project-${runNumber}`,
         base: "main",
         title: "chore(deps): upgrade provider project",
         maintainer_can_modify: true,
