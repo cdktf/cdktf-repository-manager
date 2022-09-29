8bdad5c9cf48276e2074bd7437578ed9ef6e62ca
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-09-29 18:42:54 +0200

    Fix usage of create-projen-files

--- .github/lib/create-projen-files.js
+++ .github/lib/create-projen-files.js
@@ -1,11 +1,11 @@
 
-module.exports = () => {
+module.exports = ({ providerName }) => {
     const path = require('path')
     const fs = require('fs')
     const provider = require('./main/provider.json')
-    const providerVersion = provider["${{ matrix.provider }}"]
+    const providerVersion = provider[providerName]
     const providersWithCustomRunners = require('./main/providersWithCustomRunners.json')
-    const useCustomGithubRunner = providersWithCustomRunners.includes("${{ matrix.provider }}");
+    const useCustomGithubRunner = providersWithCustomRunners.includes(providerName);
     const template = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, 'main', 'projenrc.template.js'), 'utf-8')
     const projenrc = template.replace('__PROVIDER__', providerVersion).replace('__CUSTOM_RUNNER__', useCustomGithubRunner)
     fs.writeFileSync(path.join(process.env.GITHUB_WORKSPACE, 'provider', '.projenrc.js'), projenrc)
