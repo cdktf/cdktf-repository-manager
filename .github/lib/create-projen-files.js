ba40bc1599fb158c2cbef3221ecc32ff97336ece
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-10-21 17:39:12 +0200

    fix: Use absolute paths from github workspace to require provider json

--- .github/lib/create-projen-files.js
+++ .github/lib/create-projen-files.js
@@ -1,12 +1,24 @@
-
 module.exports = ({ providerName }) => {
-    const path = require('path')
-    const fs = require('fs')
-    const provider = require('./main/provider.json')
-    const providerVersion = provider[providerName]
-    const providersWithCustomRunners = require('./main/providersWithCustomRunners.json')
-    const useCustomGithubRunner = providersWithCustomRunners.includes(providerName);
-    const template = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, 'main', 'projenrc.template.js'), 'utf-8')
-    const projenrc = template.replace('__PROVIDER__', providerVersion).replace('__CUSTOM_RUNNER__', useCustomGithubRunner)
-    fs.writeFileSync(path.join(process.env.GITHUB_WORKSPACE, 'provider', '.projenrc.js'), projenrc)
-}
+  const path = require("path");
+  const fs = require("fs");
+  const mainFolder = path.join(process.env.GITHUB_WORKSPACE, "main");
+  const provider = require(path.join(mainFolder, "provider.json"));
+  const providerVersion = provider[providerName];
+  const providersWithCustomRunners = require(path.join(
+    mainFolder,
+    "providersWithCustomRunners.json"
+  ));
+  const useCustomGithubRunner =
+    providersWithCustomRunners.includes(providerName);
+  const template = fs.readFileSync(
+    path.join(mainFolder, "projenrc.template.js"),
+    "utf-8"
+  );
+  const projenrc = template
+    .replace("__PROVIDER__", providerVersion)
+    .replace("__CUSTOM_RUNNER__", useCustomGithubRunner);
+  fs.writeFileSync(
+    path.join(process.env.GITHUB_WORKSPACE, "provider", ".projenrc.js"),
+    projenrc
+  );
+};
