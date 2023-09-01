79e5e3f9c7f811935ae7e747eee58a3ea0ad4a5c
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-09-01 12:09:19 +0200

    chore: move to old implementation

--- lib/logical-id-override.ts
+++ lib/logical-id-override.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { TerraformElement, TerraformStack } from "cdktf";
+import { TerraformElement } from "cdktf";
 import { makeUniqueId } from "cdktf/lib/private/unique";
 import { Node } from "constructs";
 
@@ -11,12 +11,9 @@ function allocateLogicalId(tfElement: TerraformElement | Node): string {
   const node = TerraformElement.isTerraformElement(tfElement)
     ? tfElement.node
     : tfElement;
-  const stack = TerraformElement.isTerraformElement(tfElement)
-    ? tfElement.cdktfStack
-    : TerraformStack.of(tfElement as any);
 
   // This is the previous behavior, which we want for now.
-  const stackIndex = node.scopes.indexOf(stack);
+  const stackIndex = 0;
 
   const components = node.scopes.slice(stackIndex + 1).map((c) => c.node.id);
   return components.length > 0 ? makeUniqueId(components) : "";
