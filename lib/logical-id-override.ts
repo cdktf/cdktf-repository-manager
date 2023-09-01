caa1ad08912083769ad96806f9e33d0cc8752bec
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-01 09:50:18 +0000

    chore: add required copyright headers
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- lib/logical-id-override.ts
+++ lib/logical-id-override.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { TerraformElement, TerraformStack } from "cdktf";
 import { makeUniqueId } from "cdktf/lib/private/unique";
 import { Node } from "constructs";
