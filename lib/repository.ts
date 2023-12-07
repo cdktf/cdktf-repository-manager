089ff30d0a5edf428a54803f1ec578c71cac3218
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-12-07 10:25:12 +0100

    chore: add no-auto-close-label

--- lib/repository.ts
+++ lib/repository.ts
@@ -59,6 +59,15 @@ export class RepositorySetup extends Construct {
       })
     );
 
+    setOldId(
+      new IssueLabel(this, `no-auto-close-label`, {
+        color: "EE2222",
+        name: "no-auto-close",
+        repository: repository.name,
+        provider,
+      })
+    );
+
     new IssueLabel(this, `auto-approve-label`, {
       color: "8BF8BD",
       name: "auto-approve",
