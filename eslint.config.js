ea827b1bd6469b7e7fb451ad0791a2bda6d64650
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-06 16:12:38 +0000

    chore: add required copyright headers
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 const { defineConfig, globalIgnores } = require("eslint/config");
 
 const typescriptEslint = require("@typescript-eslint/eslint-plugin");
